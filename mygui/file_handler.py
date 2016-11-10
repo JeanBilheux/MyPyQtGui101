e425f3a1466215b6f6d8519027cb679cfba73804
JeanBilheux <bilheuxjm@ornl.gov>
2016-11-10 16:02:32 -0500

    Loading the tiff data

--- mygui/file_handler.py
+++ mygui/file_handler.py
@@ -1,7 +1,10 @@
 from PyQt4.QtGui import QFileDialog
+from PIL import Image
 
 
 class FileHandler(object):
+
+    data = []
     
     def __init__(self, parent=None):
         self.parent = parent
@@ -9,9 +12,17 @@ class FileHandler(object):
     def browse_tiff_file(self):
         
         
-        file_name = QFileDialog.getOpenFileName(caption = "Select the TIFF file",
-                                                filter = "tiff (*.tif)")
+        file_name = str(QFileDialog.getOpenFileName(caption = "Select the TIFF file",
+                                                filter = "tiff (*.tif)"))
         
         if file_name:
             self.parent.ui.tiff_file_label.setText(file_name)
+            self.load_image(file_name)
+            
+            
+    def load_image(self, filename):
         
+        data = Image.open(filename)
+        self.data = data
+        
+            
